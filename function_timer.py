dd213beb1afa0b205e06d65fd1fb7fca590c9bd8
Fuzzy Carter <joshuaiancarter@gmail.com>
2023-05-24 17:03:38 -0700

    Update function timer to properly return the function's output from the wrapper so the result is used as intended.

--- function_timer.py
+++ function_timer.py
@@ -3,17 +3,23 @@ A simple function timer decorator.
 """
 
 import time
+from functools import wraps
 
 def function_timer(func):
     """
     A decorator that times a function.
     """
+    # @wraps(func)
     def wrapper(*args, **kwargs):
         """
         A wrapper that times a function.
         """
         start = time.time()
-        func(*args, **kwargs)
+        output = func(*args, **kwargs)
         end = time.time()
-        print(f"Function {func.__name__} took {end - start} seconds to complete.")
+
+        print(f"Function {func.__name__} took {end - start:.10f} seconds to complete.")
+
+        return output
+
     return wrapper
